4ebe5f124b596f7e64d1c4961a78d8b3d89a6198
BBernYY <66414852+BBernYY@users.noreply.github.com>
2022-07-25 16:02:06 +0200

    small fix

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from settings import model, generate
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-'+model['params'])
 res = generator(**generate)
 
-open("output.txt", "w").write(res[0]['generated_text'])
+open("output.txt", "w").write(res[0]['generated_text'].replace('�', '\''))
